0d5c9a76d86a996540178ecc19149d180b91bafd
haoguangbo <haoguangbo@yeah.net>
2016-03-05 11:30:13 +0800

    feat:input

--- maze.cpp
+++ maze.cpp
@@ -3,9 +3,24 @@
 
 #include "stdafx.h"
 
+int next(){
+
+}
+
+int back(){
+
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int maze[10][10];//maze数组用来记录迷宫的信息
+	int route[81][2];//route数组用来记录走过的路径信息
+	for (i = 0; i<10; i++)
+	for (j = 0; j<10; j++)
+		scanf("%d", &maze[i][j]);
+	//maze数组中  0表示空格  1表示墙  2表示已经走过的路径
+	
+
+	getchar();
 	return 0;
 }
-
