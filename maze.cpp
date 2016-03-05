475fdccc33badce04e9120546a141132659520a6
haoguangbo <haoguangbo@yeah.net>
2016-03-05 18:43:46 +0800

    feat:back & main

--- maze.cpp
+++ maze.cpp
@@ -24,41 +24,47 @@ int next(int x,int y){
 
 	if (maze[x][y+1]==0){//右
 		//记录这一步的信息
+		routei++;
 		route[routei][0] = x;
 		route[routei][1] = y+1;
-		routei++;
 		maze[x][y + 1]=2;//把该格子标成2
 		next(x, y + 1);//再继续走下一步
 	}
 	else if (maze[x + 1][y] == 0){//下
+		routei++;
 		route[routei][0] = x+1;
 		route[routei][1] = y;
-		routei++;
-		maze[x+1][y] = 2;//把该格子标成2
+		maze[x+1][y] = 2;
 		next(x + 1, y);
 	}
 	else if (maze[x][y-1] == 0){//左
+		routei++;
 		route[routei][0] = x;
 		route[routei][1] = y-1;
-		routei++;
-		maze[x][y - 1] = 2;//把该格子标成2
+		maze[x][y - 1] = 2;
 		next(x, y - 1);
 	}
 	else if (maze[x-1][y] == 0){//上
+		routei++;
 		route[routei][0] = x-1;
 		route[routei][1] = y;
-		routei++;
-		maze[x-1][y] = 2;//把该格子标成2
+		maze[x-1][y] = 2;
 		next(x - 1, y);
 	}
 	else//无路可走
 	{
-		back(x, y);
+		back();
 	}
 }
 
-int back(int x,int y){
-	//清除route   直到周围有路可以走
+int back(){
+	//routei倒退  直到周围有路可以走
+	do
+	{
+		routei--;
+	} while (scan(route[routei][0],route[routei][1]));
+	//继续往下走
+	next(route[routei][0], route[routei][1]);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -70,9 +76,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		scanf("%d", &maze[i][j]);
 	//maze数组中  0表示空格  1表示墙  2表示已经走过的路径
 	
+	//初始化route
 	route[0][0] = 1;
 	route[0][1] = 1;
 
+	//从1,1开始走
+	next(1, 1);
 
 	getchar();
 	return 0;
